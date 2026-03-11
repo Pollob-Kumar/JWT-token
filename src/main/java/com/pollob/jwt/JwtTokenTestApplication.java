2d101600e23eb74ae3298eb88238164638f9d01c
Pollob-Kumar <pkspollob14@gmail.com>
2026-03-11 23:27:46 +0600

    project open

---
+++ src/main/java/com/pollob/jwt/JwtTokenTestApplication.java
@@ -0,0 +1,13 @@
+package com.pollob.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtTokenTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtTokenTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pollob/jwt/JwtTokenTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollob.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTokenTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
